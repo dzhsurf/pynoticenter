5ea15ce9b847d4ea20aa9a9eee5917c41c1c4ad4
dzhsurf <dzhsurf@gmail.com>
2022-09-03 20:55:31 -0700

    update README

--- src/example/demo.py
+++ src/example/demo.py
@@ -17,27 +17,13 @@ logger = logging.getLogger(__name__)
 
 # demo
 
-
 def fn(*args: Any, **kwargs: Any):
-    desc = ""
-    for v in args:
-        desc += v + ", "
-    if desc != "":
-        desc += " "
-    desc += "kwargs: "
-    for k, v in kwargs.items():
-        desc += f"{k} = {v}, "
-    print(desc)
-
+    print(*args)
 
 def main():
-
-    print("demo start.")
-    for i in range(100000):
-        PyNotiCenter.default_center().post_task(fn, f"hello {i}", key="k", value="v")
-    time.sleep(0.5)
-    PyNotiCenter.default_center().shutdown(wait=False)
-    print("demo end.")
+    PyNotiCenter.default_center().post_task(fn, "hello world")
+    PyNotiCenter.default_center().post_task_with_delay(5, fn, "hello", "world", "delay 5s")
+    PyNotiCenter.default_center().shutdown(wait=True)
 
 
 if __name__ == "__main__":
